6d82dd4298b262c29fcb66434c7fbc653a610ddb
hasiniNeth <shasininethmini2005@gmail.com>
2025-12-09 15:57:26 +0530

    Updated caregiver_residents.php

--- caregiver/caregiver_residents.php
+++ caregiver/caregiver_residents.php
@@ -410,6 +410,7 @@ while ($alert = $alerts_result->fetch_assoc()) {
     
     <div class="sidebar-nav">
         <a href="caregiver_dashboard.php"><i class="fa-solid fa-gauge-high"></i> Dashboard</a>
+        <a href="caregiver_profile.php"><i class="fa-solid fa-user-pen"></i> My Profile</a>
         <a href="#" class="active"><i class="fa-solid fa-user-group"></i> My Residents</a>
         <a href="log_health.php"><i class="fa-solid fa-heart-pulse"></i> Log Health Data</a>
         <a href="manage_routines.php"><i class="fa-solid fa-calendar-check"></i> Manage Routines</a>
